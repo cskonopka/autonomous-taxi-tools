09687f7b09b681e44dc85d7960de77461ca15222
Christopher Konopka <cskonopka@gmail.com>
2020-03-24 03:05:09 -0400

    good beta version: mobile works with servo, LED and text submit with proper POST JSON response. Server properly works via local WiFi.

--- 01-React-Native/App.js
+++ 01-React-Native/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Button, View, Image, Text, TextInput, Alert } from 'react-native';
+import { Button, View, Image, TextInput } from 'react-native';
 
 export default class Aptiv extends Component {
+  // Defining state for TextInput
   constructor(props) {
     super(props)
     this.state = {
@@ -13,70 +14,62 @@ export default class Aptiv extends Component {
   _onPressServo(key) {
     console.log(key)
     fetch('http://10.0.0.197:5000/servo', {
-      // fetch('http://6250efad.ngrok.io/meow', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        language: key,
-        framework: 'yourOtherValue',
+        choice: key,
       }),
-    });
-    alert('servo');
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        alert(responseJson.msg);
+      })
+      .catch((error) => {
+        alert("something went wrong :/");
+      });
   }
 
   // HTTP POST to Raspberry Pi --> Control LED
   _onPressLED() {
     fetch('http://10.0.0.197:5000/led', {
-      // fetch('http://6250efad.ngrok.io/led', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        firstParam: 'yourValue',
-        secondParam: 'yourOtherValue',
-      }),
-    });
-    alert('led');
+      }
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        alert(responseJson.msg);
+      })
+      .catch((error) => {
+        alert("something went wrong :/");
+      });
   }
 
-  // HTTP POST to Raspberry Pi --> Control LED
-  _onPressBuzzer(key) {
-    fetch('http://10.0.0.197:5000/buzzer', {
-      // fetch('http://6250efad.ngrok.io/meow', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        language: key,
-        framework: 'yourOtherValue',
-      }),
-    });
-    alert('servo');
-  }
-
-  buttonClickListener = () => {
+  // HTTP POST to Raspberry Pi --> Passcode submission
+  _onPressPasscode = () => {
     const { TextInputValue } = this.state;
-    // Alert.alert(TextInputValue);
     fetch('http://10.0.0.197:5000/passcode', {
-      // fetch('http://6250efad.ngrok.io/meow', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        language: TextInputValue,
-        framework: 'yourOtherValue',
-      }),
+        answer: TextInputValue
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      alert(responseJson.msg);
+    })
+    .catch((error) => {
+      alert("something went wrong :/");
     });
-    alert('passcode');
   }
 
   render() {
@@ -89,19 +82,16 @@ export default class Aptiv extends Component {
         </Image>
         <View style={styles.servoContainer} >
           <Button
-            // style={{ flex: 1 }}
             title="Low"
-            onPress={() => this._onPressServo('0')}
+            onPress={() => this._onPressServo('Low')}
           />
           <Button
-            // style={{ flex: 1 }}
-            title="Med"
-            onPress={() => this._onPressServo('1')}
+            title="Medium"
+            onPress={() => this._onPressServo('Medium')}
           />
           <Button
-            // style={{ flex: 1 }}
             title="High"
-            onPress={() => this._onPressServo('2')}
+            onPress={() => this._onPressServo('High')}
           />
         </View>
         <View style={styles.ledContainer} >
@@ -112,34 +102,18 @@ export default class Aptiv extends Component {
         </View>
         <View style={styles.passcodeContainer} >
           <TextInput
-            style={{ height: 45, borderColor: "gray", borderWidth: 2 }}
+            style={{ height: 45, borderColor: "black", borderWidth: 2 }}
             placeholder="  Enter the passcode"
-            //set the value in state.
             onChangeText={TextInputValue => this.setState({ TextInputValue })}
-            // Making the Under line Transparent.
             underlineColorAndroid="transparent"
           />
           <Button
-            onPress={this.buttonClickListener}
-            title="Submit passcode"
+            onPress={this._onPressPasscode}
+            title="Submit"
           />
         </View>
-        {/* <View style={styles.buzzerContainer} >
-          <Button
-            // style={{ flex: 1 }}
-            title="Low"
-            onPress={() => this._onPressBuzzer('0')}
-          />
-          <Button
-            // style={{ flex: 1 }}
-            title="Med"
-            onPress={() => this._onPressBuzzer('1')}
-          />
-        </View> */}
         <View style={{ flex: 1, backgroundColor: '#FFFFFF' }} />
         <View style={{ flex: 1, backgroundColor: '#FFFFFF' }} />
-
-
       </View>
     );
   }
@@ -178,24 +152,5 @@ const styles = {
   passcodeContainer: {
     flex: 5,
     backgroundColor: '#FFFFFF'
-  },
-  buzzerContainer: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  elementsContainer: {
-    backgroundColor: '#FFFFFF',
-    marginLeft: 24,
-    marginRight: 24,
-    marginBottom: 24
-  },
-  headerText: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-    fontWeight: "bold"
   }
 }
--- 02-RaspberryPi/aptiv-server.py
+++ 02-RaspberryPi/aptiv-server.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask import request
+from flask import jsonify
 
 # Raspberry Pi
 import RPi.GPIO as GPIO
@@ -27,117 +28,103 @@ p = GPIO.PWM(17, 50)
 GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW)
 
 # LED setup
-GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW)  # press
+GPIO.setup(2, GPIO.OUT, initial=GPIO.LOW)  # passcode yes
+GPIO.setup(3, GPIO.OUT, initial=GPIO.LOW)  # passcode no
 
 # ~~~~~~~~~~~~~~~ RPi GPIO Functions ~~~~~~~~~~~~~~~
 # servo_routine --> Control servo via GPIO
 def servo_routine():
-        data = request.get_json()
-        print(data['language'])
-        print(data['framework'])
-        data = request.get_json()
-        if data['language'] == '0':
-                print('low')
-                p.start(2.5)
-                p.ChangeDutyCycle(2.5)
-                time.sleep(1)
-                p.stop()
-        elif data['language'] == '1':
-                print('med')
-                p.start(5)
-                p.ChangeDutyCycle(7.5)
-                time.sleep(0.5)
-                p.stop()
-        elif data['language'] == '2':
-                print('high')
-                p.start(7)
-                p.ChangeDutyCycle(12.5)
-                time.sleep(0.5)
-                p.stop()
+    data = request.get_json()
+    print(data['choice'])
+    data = request.get_json()
+    if data['choice'] == 'Low':
+        print('low')
+        p.start(2.5)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(1)
+        p.stop()
+    elif data['choice'] == 'Medium':
+        print('med')
+        p.start(5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.stop()
+    elif data['choice'] == 'High':
+        print('high')
+        p.start(7)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(0.5)
+        p.stop()
 
 # led_routine --> Control LEDs via GPIO
 def led_routine():
-        GPIO.output(14, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(14, GPIO.LOW)
-        time.sleep(1)
-
-# buzzer_routine --> Control LEDs via GPIO
-def buzzer_routine():
-        data = request.get_json()
-        print(data['language'])
-        print(data['framework'])
-        data = request.get_json()
-        if data['language'] == '0':
-                print('low')
-                GPIO.output(27, GPIO.HIGH)
-                time.sleep(0.1)
-                GPIO.output(27, GPIO.LOW)
-                time.sleep(0.1)
-        elif data['language'] == '1':
-                for x in range(0, 3):
-                        GPIO.output(27, GPIO.HIGH)
-                        time.sleep(0.1)
-                        GPIO.output(27, GPIO.LOW)
-                        time.sleep(0.1)
-						
-# passcode_routine --> Verify passcode with a buzzer
-def passcode_routine():
-    data = request.get_json()
-    print(data['language'])
-    print(data['framework'])
-    data = request.get_json()
-    if data['language'] == '1994':
-        GPIO.output(27, GPIO.HIGH)
-        time.sleep(2)
-        GPIO.output(27, GPIO.LOW)
-        time.sleep(0.1)
-    else:
-        for x in range(0, 3):
-            GPIO.output(27, GPIO.HIGH)
-            time.sleep(0.1)
-            GPIO.output(27, GPIO.LOW)
-            time.sleep(0.1)
+    GPIO.output(14, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(14, GPIO.LOW)
+    time.sleep(1)
 
 # ~~~~~~~~~~~~~~~ API ~~~~~~~~~~~~~~~
 # ENDPOINT --> Index test
 @app.route('/')
 def index():
-        return 'this is a beginning'
+    return 'this is a beginning'
 
 # ENDPOINT --> Servo
 @app.route('/servo', methods=['GET', 'POST'])
 def servo():
-        data = request.get_json()
-        # Start servo operation
-        servo_routine()
-        # Return prompt
-        return 'the heat is set to ' + data['language']
+    data = request.get_json()
+    # Start servo operation
+    servo_routine()
+    # Return prompt
+    return jsonify(
+        msg='The heat is set to ' + data['choice']
+    )
 
 # ENDPOINT --> LED
 @app.route('/led', methods=['POST'])
 def led():
-        # Start LED operation
-        led_routine()
-        # Return prompt
-        return 'Thank you for riding with us!'
-
-# ENDPOINT --> Buzzer
-@app.route('/buzzer', methods=['POST'])
-def buzzer():
-        # Start LED operation
-        buzzer_routine()
-        # Return prompt
-        return 'buzzed!'
+    # Start LED operation
+    led_routine()
+    # Return prompt
+    return jsonify(
+        msg='Thank you for riding with us!'
+    )
 
 # ENDPOINT --> Passcode
 @app.route('/passcode', methods=['POST'])
 def passcode():
-    # Start LED operation
-    passcode_routine()
-    # Return prompt
-    return 'passcode there'
+	data = request.get_json()
+	if data['answer'] == '1994':
+		GPIO.output(27, GPIO.HIGH)
+		GPIO.output(2, GPIO.HIGH)
+		time.sleep(0.5)
+		GPIO.output(27, GPIO.LOW)
+		GPIO.output(2, GPIO.LOW)
+		time.sleep(0.1)
+		return jsonify(msg='Success!')
+	elif data['answer'] == 'aptiv':
+		for x in range(0, 10):
+			GPIO.output(2, GPIO.HIGH)
+			GPIO.output(3, GPIO.LOW)
+			time.sleep(0.1)
+			GPIO.output(2, GPIO.LOW)
+			GPIO.output(3, GPIO.HIGH)
+			time.sleep(0.1)
+			GPIO.output(2, GPIO.LOW)
+			GPIO.output(3, GPIO.LOW)
+			time.sleep(0.1)
+		return jsonify(msg='EASTER EGG')
+	else:
+		for x in range(0, 3):
+			GPIO.output(27, GPIO.HIGH)
+			GPIO.output(3, GPIO.HIGH)
+			time.sleep(0.1)
+			GPIO.output(27, GPIO.LOW)
+			GPIO.output(3, GPIO.LOW)
+			time.sleep(0.1)
+		return jsonify(msg='INCORRECT')
 
 # ~~~~~~~~~~~~~~~ Flask ~~~~~~~~~~~~~~~
 if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
