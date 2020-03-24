0e5ab148419f9885b7e06b67c91b5fa08f991c21
Christopher Konopka <cskonopka@gmail.com>
2020-03-24 00:32:13 -0400

    stable version of phone app

--- 01-React-Native/App.js
+++ 01-React-Native/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Switch, StyleSheet, View, Image } from 'react-native';
+import { Button, View, Image } from 'react-native';
 
 export default class Aptiv extends Component {
   // HTTP POST to Raspberry Pi --> Servo
@@ -41,70 +41,73 @@ export default class Aptiv extends Component {
     return (
       <View style={styles.container}>
         <Image style={styles.image}
+          resizeMode="contain"
+          resizeMethod="resize"
           source={{ uri: 'https://i.ibb.co/7twxPvD/aptiv.png' }}>
         </Image>
-        <View style={styles.buttonContainerServo}>
-          <Button
-            title="Low"
-            onPress={() => this._onPressServo('0')}
-          />
-          <Button
-            title="Med"
-            onPress={() => this._onPressServo('1')}
-          />
-          <Button
-            title="High"
-            onPress={() => this._onPressServo('2')}
-          />
+          <View style={styles.servoContainer} >
+            <Button style={{ flex: 1 }}
+              title="Low"
+              onPress={() => this._onPressServo('0')}
+            />
+            <Button
+              style={{ flex: 1 }}
+              title="Med"
+              onPress={() => this._onPressServo('1')}
+            />
+            <Button
+              style={{ flex: 1 }}
+              title="High"
+              onPress={() => this._onPressServo('2')}
+            />
+          </View>
+          <View style={styles.ledContainer} >
+            <Button
+              onPress={this._onPressLED}
+              title="LED"
+            />
+          </View>
+          <View style={{ flex: 1, backgroundColor: '#FFFFFF' }} />
+          <View style={{ flex: 3, backgroundColor: '#FFFFFF' }} />
         </View>
-
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressLED}
-            title="LED"
-          />
-        </View>
-        <Switch></Switch>
-      </View >
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
+    marginTop: 48,
+    justifyContent:'center',
+    alignContent:'center',
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'column',
-        resizeMode: "center",
+    backgroundColor: '#FFFFFF',
+    marginLeft: 24,
+    marginRight: 24,
+    marginBottom: 24
   },
   image: {
-    flexGrow: 1,
-    // margin: 74,
-    resizeMode: "center",
-    // borderTopWidth: 1,
-    // alignItems: 'center',
+    height: 100, 
+    width: 300, 
+    backgroundColor: '#FFFFFF',
+    resizeMode: 'contain', 
+    alignContent:'center', 
+    justifyContent:'center'
   },
-  buttonContainerServo: {
-    flex: 0.9,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    margin: 75,
-    // paddingVertical: -10
+  servoContainer: {
+    flex: 1, 
+    backgroundColor: '#FFFFFF', 
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'center',
   },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-
-    // width: "100%",
-    // margin: 13,
-    // paddingBottom: 10
+  ledContainer: {
+    flex: 1, 
+    backgroundColor: '#FFFFFF'
   },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+  elementsContainer: {
+    backgroundColor: '#FFFFFF',
+    marginLeft: 24,
+    marginRight: 24,
+    marginBottom: 24
   }
-
-});
+}
