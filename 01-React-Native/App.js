400415501949941f8ce5264e4de6139c7a8bede7
Christopher Konopka <cskonopka@gmail.com>
2020-03-24 01:51:09 -0400

    added passcode to the mobile and server

--- 01-React-Native/App.js
+++ 01-React-Native/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-import { Button, View, Image } from 'react-native';
+import { Button, View, Image, Text, TextInput, Alert } from 'react-native';
 
 export default class Aptiv extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      TextInputValue: ''
+    }
+  }
+
   // HTTP POST to Raspberry Pi --> Determine servo position: 'Low', 'Med', 'High'
   _onPressServo(key) {
     console.log(key)
@@ -17,7 +24,7 @@ export default class Aptiv extends Component {
         framework: 'yourOtherValue',
       }),
     });
-    alert('servo')
+    alert('servo');
   }
 
   // HTTP POST to Raspberry Pi --> Control LED
@@ -34,7 +41,42 @@ export default class Aptiv extends Component {
         secondParam: 'yourOtherValue',
       }),
     });
-    alert('led')
+    alert('led');
+  }
+
+  // HTTP POST to Raspberry Pi --> Control LED
+  _onPressBuzzer(key) {
+    fetch('http://10.0.0.197:5000/buzzer', {
+      // fetch('http://6250efad.ngrok.io/meow', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        language: key,
+        framework: 'yourOtherValue',
+      }),
+    });
+    alert('servo');
+  }
+
+  buttonClickListener = () => {
+    const { TextInputValue } = this.state;
+    // Alert.alert(TextInputValue);
+    fetch('http://10.0.0.197:5000/passcode', {
+      // fetch('http://6250efad.ngrok.io/meow', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        language: TextInputValue,
+        framework: 'yourOtherValue',
+      }),
+    });
+    alert('passcode');
   }
 
   render() {
@@ -43,33 +85,62 @@ export default class Aptiv extends Component {
         <Image style={styles.image}
           resizeMode="contain"
           resizeMethod="resize"
-          source={{ uri: 'https://i.ibb.co/7twxPvD/aptiv.png' }}>
+          source={{ uri: 'https://i.ibb.co/nsqfBcb/aptiv.png' }}>
         </Image>
-          <View style={styles.servoContainer} >
-            <Button style={{ flex: 1 }}
-              title="Low"
-              onPress={() => this._onPressServo('0')}
-            />
-            <Button
-              style={{ flex: 1 }}
-              title="Med"
-              onPress={() => this._onPressServo('1')}
-            />
-            <Button
-              style={{ flex: 1 }}
-              title="High"
-              onPress={() => this._onPressServo('2')}
-            />
-          </View>
-          <View style={styles.ledContainer} >
-            <Button
-              onPress={this._onPressLED}
-              title="LED"
-            />
-          </View>
-          <View style={{ flex: 1, backgroundColor: '#FFFFFF' }} />
-          <View style={{ flex: 3, backgroundColor: '#FFFFFF' }} />
+        <View style={styles.servoContainer} >
+          <Button
+            // style={{ flex: 1 }}
+            title="Low"
+            onPress={() => this._onPressServo('0')}
+          />
+          <Button
+            // style={{ flex: 1 }}
+            title="Med"
+            onPress={() => this._onPressServo('1')}
+          />
+          <Button
+            // style={{ flex: 1 }}
+            title="High"
+            onPress={() => this._onPressServo('2')}
+          />
         </View>
+        <View style={styles.ledContainer} >
+          <Button
+            onPress={this._onPressLED}
+            title="LED"
+          />
+        </View>
+        <View style={styles.passcodeContainer} >
+          <TextInput
+            style={{ height: 45, borderColor: "gray", borderWidth: 2 }}
+            placeholder="  Enter the passcode"
+            //set the value in state.
+            onChangeText={TextInputValue => this.setState({ TextInputValue })}
+            // Making the Under line Transparent.
+            underlineColorAndroid="transparent"
+          />
+          <Button
+            onPress={this.buttonClickListener}
+            title="Submit passcode"
+          />
+        </View>
+        {/* <View style={styles.buzzerContainer} >
+          <Button
+            // style={{ flex: 1 }}
+            title="Low"
+            onPress={() => this._onPressBuzzer('0')}
+          />
+          <Button
+            // style={{ flex: 1 }}
+            title="Med"
+            onPress={() => this._onPressBuzzer('1')}
+          />
+        </View> */}
+        <View style={{ flex: 1, backgroundColor: '#FFFFFF' }} />
+        <View style={{ flex: 1, backgroundColor: '#FFFFFF' }} />
+
+
+      </View>
     );
   }
 }
@@ -77,8 +148,8 @@ export default class Aptiv extends Component {
 const styles = {
   container: {
     marginTop: 48,
-    justifyContent:'center',
-    alignContent:'center',
+    justifyContent: 'center',
+    alignContent: 'center',
     flex: 1,
     backgroundColor: '#FFFFFF',
     marginLeft: 24,
@@ -86,28 +157,45 @@ const styles = {
     marginBottom: 24
   },
   image: {
-    height: 100, 
-    width: 300, 
+    height: 100,
+    width: 300,
     backgroundColor: '#FFFFFF',
-    resizeMode: 'contain', 
-    alignContent:'center', 
-    justifyContent:'center'
+    resizeMode: 'contain',
+    alignContent: 'center',
+    justifyContent: 'center'
   },
   servoContainer: {
-    flex: 1, 
-    backgroundColor: '#FFFFFF', 
-    flexDirection: 'row', 
-    alignItems: 'center', 
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'center',
   },
   ledContainer: {
-    flex: 1, 
+    flex: 1,
     backgroundColor: '#FFFFFF'
+  },
+  passcodeContainer: {
+    flex: 5,
+    backgroundColor: '#FFFFFF'
+  },
+  buzzerContainer: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   elementsContainer: {
     backgroundColor: '#FFFFFF',
     marginLeft: 24,
     marginRight: 24,
     marginBottom: 24
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    fontWeight: "bold"
   }
 }
--- 02-RaspberryPi/aptiv-server.py
+++ 02-RaspberryPi/aptiv-server.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask import request
 
-# Raspberry Pi 
+# Raspberry Pi
 import RPi.GPIO as GPIO
 
 # General
@@ -20,14 +20,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Servo setup
-servoPIN=17
-GPIO.setup(servoPIN, GPIO.OUT)
-p = GPIO.PWM(servoPIN, 50)
+GPIO.setup(17, GPIO.OUT)
+p = GPIO.PWM(17, 50)
 
 # Buzzer setup
-buzzPin=11
-GPIO.setup(buzzPin, GPIO.OUT)   # Set BeepPin's mode is output
-GPIO.output(buzzPin, GPIO.HIGH) # Set BeepPin high(+3.3V) to off beep
+GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW)
 
 # LED setup
 GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW)
@@ -35,28 +32,28 @@ GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW)
 # ~~~~~~~~~~~~~~~ RPi GPIO Functions ~~~~~~~~~~~~~~~
 # servo_routine --> Control servo via GPIO
 def servo_routine():
-	data = request.get_json()
-	print(data['language'])
-	print(data['framework'])
-	data = request.get_json()
-	if data['language'] == '0':
-		print('low')
-		p.start(2.5)
-		p.ChangeDutyCycle(2.5)
-		time.sleep(1)
-		p.stop()
-	elif data['language'] == '1':
-		print('med')
-		p.start(5)
-		p.ChangeDutyCycle(7.5)
-		time.sleep(0.5)
-		p.stop()
-	elif data['language'] == '2':
-		print('high')
-		p.start(7)
-		p.ChangeDutyCycle(12.5)
-		time.sleep(0.5)
-		p.stop()
+        data = request.get_json()
+        print(data['language'])
+        print(data['framework'])
+        data = request.get_json()
+        if data['language'] == '0':
+                print('low')
+                p.start(2.5)
+                p.ChangeDutyCycle(2.5)
+                time.sleep(1)
+                p.stop()
+        elif data['language'] == '1':
+                print('med')
+                p.start(5)
+                p.ChangeDutyCycle(7.5)
+                time.sleep(0.5)
+                p.stop()
+        elif data['language'] == '2':
+                print('high')
+                p.start(7)
+                p.ChangeDutyCycle(12.5)
+                time.sleep(0.5)
+                p.stop()
 
 # led_routine --> Control LEDs via GPIO
 def led_routine():
@@ -67,22 +64,40 @@ def led_routine():
 
 # buzzer_routine --> Control LEDs via GPIO
 def buzzer_routine():
-	data = request.get_json()
-	print(data['language'])
-	print(data['framework'])
-	data = request.get_json()
-	if data['language'] == '0':
-		print('low')
-		GPIO.output(11, GPIO.LOW)
-		time.sleep(0.1)
-		GPIO.output(11, GPIO.HIGH)
-		time.sleep(0.1)
-	elif data['language'] == '1':
-		for x in range(0, 3):
-			GPIO.output(11, GPIO.LOW)
-			time.sleep(0.1)
-			GPIO.output(11, GPIO.HIGH)
-			time.sleep(3)
+        data = request.get_json()
+        print(data['language'])
+        print(data['framework'])
+        data = request.get_json()
+        if data['language'] == '0':
+                print('low')
+                GPIO.output(27, GPIO.HIGH)
+                time.sleep(0.1)
+                GPIO.output(27, GPIO.LOW)
+                time.sleep(0.1)
+        elif data['language'] == '1':
+                for x in range(0, 3):
+                        GPIO.output(27, GPIO.HIGH)
+                        time.sleep(0.1)
+                        GPIO.output(27, GPIO.LOW)
+                        time.sleep(0.1)
+						
+# passcode_routine --> Verify passcode with a buzzer
+def passcode_routine():
+    data = request.get_json()
+    print(data['language'])
+    print(data['framework'])
+    data = request.get_json()
+    if data['language'] == '1994':
+        GPIO.output(27, GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(27, GPIO.LOW)
+        time.sleep(0.1)
+    else:
+        for x in range(0, 3):
+            GPIO.output(27, GPIO.HIGH)
+            time.sleep(0.1)
+            GPIO.output(27, GPIO.LOW)
+            time.sleep(0.1)
 
 # ~~~~~~~~~~~~~~~ API ~~~~~~~~~~~~~~~
 # ENDPOINT --> Index test
@@ -93,28 +108,36 @@ def index():
 # ENDPOINT --> Servo
 @app.route('/servo', methods=['GET', 'POST'])
 def servo():
-	data = request.get_json()
-	# Start servo operation
-	servo_routine()
-	# Return prompt
-	return 'the heat is set to ' + data['language']
+        data = request.get_json()
+        # Start servo operation
+        servo_routine()
+        # Return prompt
+        return 'the heat is set to ' + data['language']
 
 # ENDPOINT --> LED
 @app.route('/led', methods=['POST'])
 def led():
-	# Start LED operation
-	led_routine()
-	# Return prompt
-	return 'Thank you for riding with us!'
+        # Start LED operation
+        led_routine()
+        # Return prompt
+        return 'Thank you for riding with us!'
 
+# ENDPOINT --> Buzzer
 @app.route('/buzzer', methods=['POST'])
 def buzzer():
-	# Start LED operation
-	buzzer_routine()
-	# Return prompt
-	return 'buzzed!'
+        # Start LED operation
+        buzzer_routine()
+        # Return prompt
+        return 'buzzed!'
+
+# ENDPOINT --> Passcode
+@app.route('/passcode', methods=['POST'])
+def passcode():
+    # Start LED operation
+    passcode_routine()
+    # Return prompt
+    return 'passcode there'
 
 # ~~~~~~~~~~~~~~~ Flask ~~~~~~~~~~~~~~~
 if __name__ == '__main__':
         app.run(debug=True, host='0.0.0.0')
-
